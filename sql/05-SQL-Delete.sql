65cb7f568f540c6dcab3c18711aaf47b942394ff
Aleksandar Dimic <aleksandar.dimic@cubes.rs>
2017-10-30 16:11:59 +0100

    Cas 24 - Kraj

--- sql/05-SQL-Delete.sql
+++ sql/05-SQL-Delete.sql
@@ -1,5 +1,24 @@
 -- Napisati upit koji bise proizvod sa ID-jem 32
+DELETE
+FROM
+	products
+WHERE
+	id = 32;
 
--- Napisati upit koji brise sve proizvode iz kategorije 'Ves masina'
+-- Napisati upit koji brise sve proizvode iz kategorije 'Ves Masina'
+
+DELETE
+FROM
+	products
+WHERE 
+	category = 'Ves Masina';
 
 -- Napisati upit koji brise proizvode sa ID-jevima 13, 23, 33
+DELETE
+FROM
+	products
+WHERE
+	id IN(12, 23, 33);
+
+-- CRUD - Create Read Update Delete
+
