7e626414dc4368e740e15004d6e371222a49edb7
Aleksandar Dimic <aleksandar.dimic@cubes.rs>
2017-11-13 16:10:45 +0100

    Cas 30 - Kraj

--- models/m_products.php
+++ models/m_products.php
@@ -97,3 +97,11 @@ function productsGetCount() {
 	
 	return dbFetchColumn($query);
 }
+
+function productsUpdatePhotoFileName($id, $photoFileName) {
+	$query = "UPDATE products "
+			. "SET photo_filename = '". dbEscape($photoFileName) ."' "
+			. "WHERE id = '" . dbEscape($id) . "'";
+	
+	return dbQuery($query);
+}
